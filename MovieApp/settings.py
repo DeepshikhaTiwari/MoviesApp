c279e4f90ef64397384f6355b9fbb100dcfa8fa4
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-23 14:15:10 +0530

    Change settings and create procfile and requirements to deploy in heroku

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = '47uqb3dr3bw%7!t(@@516-)y1@hfflotuwrh1yv+1a)rst82at'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moviesapp123.herokuapp', '127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
