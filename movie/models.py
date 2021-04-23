090555a0b9d3bfa2d7e2f48402f68604222a5938
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-23 13:04:05 +0530

    Create models for movies app

--- movie/models.py
+++ movie/models.py
@@ -7,6 +7,7 @@ class director(models.Model):
     phone_no = models.CharField(max_length=15)
     dob = models.DateField(auto_now=False, null=True)
     website = models.URLField(max_length=200, null=True)
+    profile_url = models.URLField(max_length=500, null=True)
 
     GENDER_CHOICES = (
         (0, 'male'),
@@ -34,6 +35,9 @@ class studio(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('studio-detail', args=[str(self.id)])
+
 
 class Genre(models.Model):
     title = models.CharField(max_length=50, help_text='Enter genre for movie eg: Action, comedy etc')
@@ -45,12 +49,14 @@ class Genre(models.Model):
 
 class Movie(models.Model):
     title = models.CharField(max_length=50)
-    prefix = models.CharField(max_length=100)
+    prefix = models.CharField(max_length=100, null=True, blank=True)
     subtitle = models.CharField(max_length=100, help_text='Enter subtitles for movie ex: english,hindi etc')
     slug = models.SlugField(max_length=250, blank=True, null=True)
     directors = models.ForeignKey('director', on_delete=models.SET_NULL, null=True)
     studio = models.ForeignKey('studio', on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this movie')
+    movie_url = models.URLField(max_length=500, null=True)
+    img_url = models.URLField(max_length=500)
     asin = models.CharField(max_length=20, unique=True, help_text='Enter Amazon Standard Identification Number')
 
     def __str__(self):
