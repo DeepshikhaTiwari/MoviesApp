dd8cdc4ef7974ead82bfd35f8873e43b63e6e61a
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-23 13:08:05 +0530

    Add path for movies urls

--- MovieApp/urls.py
+++ MovieApp/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('movie/', include('movie.urls')),
+    path('', RedirectView.as_view(url='movie/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+  ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
